682f0f24b6d5c2b6278e3bfc132dae02c2a655ee
Adrien Mahieux <adrien@mahieux.net>
2015-10-08 19:44:00 +0100

    [Add] Added comments on requirements for latest_build.sh

--- src/crash/latest_build.sh
+++ src/crash/latest_build.sh
@@ -4,8 +4,9 @@ declare MYSELF="$(readlink -f $0)"
 declare MYPATH="${MYSELF%/*}"
 declare MYSRCS="$MYPATH/src"
 
-#Require :
-# wget, gcc, zlib-devel, ncurses-devel, bison
+#Require:    wget, gcc, zlib-devel, ncurses-devel, bison
+#Recommands: lzo-devel, snappy-devel
+#
 
 # Do the build
 make -C "$MYSRCS" -j "$(grep -c processor /proc/cpuinfo)"
